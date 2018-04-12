fcd91d2e03667a0ebd854c7d27f197ff06432876
key_life <m13760315374@163.com>
2018-04-12 09:52:12 +0800

    '1.0.0'

---
+++ pages/carmer/index.js
@@ -0,0 +1,93 @@
+// pages/carmer/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  takeVideo() {
+    var that = this;
+    this.ctx.startRecord({
+      success: function () {
+        console.log('start')
+      }
+    })
+    setTimeout(function () {
+      that.ctx.stopRecord({
+        success: function (res) {
+          console.log(res.tempVideoPath)
+        }
+      })
+    }, 5000)
+  },
+  takePhoto() {
+    this.ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        this.setData({
+          src: res.tempImagePath
+        })
+      }
+    })
+  },
+  error(e) {
+    console.log(e.detail)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.ctx = wx.createCameraContext()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
